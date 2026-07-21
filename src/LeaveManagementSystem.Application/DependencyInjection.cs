5cbda4bc7b8bcc8a524885e6c1cb0b36b742adc4
Irem Ozturk <iremozturk36@gmail.com>
2026-07-22 00:35:21 +0300

    feat(phase3): add CQRS foundation setup
    - Add MediatR and FluentValidation packages to the Application layer
    - Register handlers and validators through assembly scanning
    - Register the Application layer in WebAPI startup

---
+++ src/LeaveManagementSystem.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LeaveManagementSystem.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        var applicationAssembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuration =>
+            configuration.RegisterServicesFromAssembly(applicationAssembly));
+
+        services.AddValidatorsFromAssembly(applicationAssembly);
+
+        return services;
+    }
+}
